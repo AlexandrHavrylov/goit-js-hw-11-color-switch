cffa71f87d5147a2d5ee40fbb916c4ef8ee42b01
Sasha Havrylov <sashagavrilov1995@gmail.com>
2021-06-30 22:31:35 +0300

    убирает лишний консоль лог

--- src/index.js
+++ src/index.js
@@ -1,16 +1,14 @@
 import colors from './colors.json'
 
-console.log(colors)
+
 
 const body = document.querySelector('body')
 const startBtn = document.querySelector('button[data-action="start"]')
 const stopBtn = document.querySelector('button[data-action="stop"]')
 
-
 const randomIntegerFromInterval = (min, max) => {
   return Math.floor(Math.random() * (max - min + 1) + min);
 };
-
 
 
 let changeColor = null;
